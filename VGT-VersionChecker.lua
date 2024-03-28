7ed7b7c1e6fcc27cf2e0b67a2930d48b58214571
Elite747 <48695653+Elite747@users.noreply.github.com>
2024-03-27 18:41:22 -0700

    Removed backward compatibility with old addon versions.

--- VGT-VersionChecker.lua
+++ VGT-VersionChecker.lua
@@ -3,14 +3,8 @@ local versionChecker = VGT:NewModule("versionChecker")
 
 versionChecker.latestVersion = VGT.version
 
-function versionChecker:GET_VERSION(_, sender, versionDeprecated)
+function versionChecker:GET_VERSION(_, sender)
   local version = VGT.version or ""
-  if versionDeprecated then
-    -- Addon version < 99 sends the version in GET_VERSION
-    -- If this is the case, a fake version number with the old
-    -- style of game-version-prefixing so that ordinal checking works
-    version = "30501." .. version
-  end
   VGT.LogTrace("Responding to version request from %s with version %s", sender, version)
   VGT:SendPlayerAddonCommand(sender, VGT.Commands.VERSION_RESPOND, version)
 end
